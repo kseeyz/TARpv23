e4deaa344305eee96d02c82157511cf8b4206793
neflor1n <153904907+neflor1n@users.noreply.github.com>
2024-02-02 10:25:36 +0200

    Add files via upload

--- kontrolltoo_kordused.py
+++ kontrolltoo_kordused.py
@@ -1,57 +1,95 @@
-            #1
-try:
-    n = int(input("Sisestage jäneste arv (1 kuni 9):"))
+from random import *
+import random
 
-    if 1 <= n <= 9:
-        for i in range(n):
-            print("   (\\_/)")
-            print("   (o o)")
-            print("   / | \\" if i < n - 1 else "   / | ")
-            print()
-except:
-    print("Kirjutage arv vahemikus 1 kuni 9")
+while True:
+    try:
+        K = int(input("Mitu kotleti sul on?: "))
+        if K > 0:
+            break
+    except ValueError:
+        print("Vale tuup")
+while True:
+    try:
+        M = int(input("Mitu kotleti uhel pannil?: "))
+        if M > 0:
+            break
+    except ValueError:
+        print("Vale tuup")
+pann = 0
+while K > 0:
+    K -= M
+    pann += 1
+    print(f'praetud: {pann} tk')
+    if K<M:
+        pann += 1
+        print(f'praetud: {pann} tk')
+print(f'Kokku oli praetid: {pann} pannid')
+print()
+
+
+            #1
+
+# while True:
+#     try:
+#         n = int(input("Sisestage jäneste arv (1 kuni 9):"))
+#         if 1 <= n <= 9:
+#             break
+#    except:
+#         print("Valju tuup")
+#
+#
+# for i in range(n):
+#     print(' /V\ '.center(10, ' '), end = "")
+# print()
+# for i in range(n):
+#     print(" (o o) ".center(10, ' '), end = "")
+# print()
+#
+# for i in range(n):
+#     print("   / | \\" if i < n - 1 else "   / | ")
+
 
 
             #2
-try:
-    L = int(input("Sisestage L väärtus: "))
-    sum_tulemus = 0
-    kalkulaator = " "
-    for i in range(L + 1):
+
+L = int(input("Sisestage L väärtus: "))
+sum_tulemus = 0
+kalkulaator = " "
+for i in range(L + 1):
+    try:
         sum_tulemus += 1
         kalkulaator += f'{i} + '
 
-    kalkulaator = kalkulaator[:-2]
-    result_täisarv = f"{kalkulaator}= {sum_tulemus}"
-    print(result_täisarv)
-
-except:
-            print("Sisestage oma arv, palun")
+        kalkulaator = kalkulaator[:-2]
+        result_taisarv = f"{kalkulaator}= {sum_tulemus}"
+        print(result_taisarv)
+    except:
+        print("Sisestage oma arv, palun")
 
 
             #3
 
-import random
-try:
-    salajane_number = random.randint(0, 100)
-    katsete_piirang = 10
 
-    print("Arva ära arv vahemikus 0 kuni 100. Sul on 10 katset.")
+salajane_number = random.randint(0, 100)
+katsete_piirang = 10
 
-    for katsete in range(1, katsete_piirang + 1):
-        kasutaja_arvamine = int(input(f"Katse {katsete}: sisestage oma valik: "))
+print("Arva ära arv vahemikus 0 kuni 100. Sul on 10 katset.")
 
-        if kasutaja_arvamine == salajane_number:
-            print(f"Palju õnne! Arvasite arvu {salajane_number} pärast {katsete} katset ära.")
-            break
-        elif kasutaja_arvamine < salajane_number:
-            print("Rohkem. Proovi uuesti.")
-        else:
-            print("Vähem. Proovi uuesti.")
+for katsete in range(1, katsete_piirang + 1):
+
+    kasutaja_arvamine = int(input(f"Katse {katsete}: sisestage oma valik: "))
+
+    if kasutaja_arvamine == salajane_number:
+        print(f"Palju õnne! Arvasite arvu {salajane_number} pärast {katsete} katset ära.")
+        break
+    elif kasutaja_arvamine < salajane_number:
+        print("Rohkem. Proovi uuesti.")
     else:
-        print(f"Olete kõik katsed ammendanud. Peidetud number oli {salajane_number}")
-except:
-    print("Valeteade")
+        print("Vähem. Proovi uuesti.")
+else:
+    print(f"Olete kõik katsed ammendanud. Peidetud number oli {salajane_number}")
+
+
 
             #4
 
@@ -62,34 +100,37 @@ try:
 except:
     print("Kirjutage täisarv")
 
-            #4.2
 
 a = int(input("Sisestage täisarv: "))
 b = 0
 while a > 0:
-    number = a % 10
-    a = a // 10
-    b = b * 10
-    b += number
-print("*Ümberpööratud* number", str(b))
-
+    try:
+        number = a % 10
+        a = a // 10
+        b = b * 10
+        b += number
+        print("*Ümberpööratud* number", str(b))
+    except TypeError:
+        print("Vale tuup")
             #5
 
-try:
-    arv = int(input('Sisestage arv: '))
-    sum = 0
-    arvu_korrutis = 1
-    while arv > 0:
+
+arv = int(input('Sisestage arv: '))
+sum = 0
+arvu_korrutis = 1
+
+while arv > 0:
+    try:
         sum += arv % 10
         arvu_korrutis *= arv % 10
         arv = arv//10
 
 
-    print(f'Sum =')
-    print("Summa:", sum)
-    print("Korrutis:", arvu_korrutis)
-except:
-    print("Vaja numbrit")
+        print(f'Sum =')
+        print("Summa:", sum)
+        print("Korrutis:", arvu_korrutis)
+    except:
+        print("Vaja numbrit")
 
 
 
